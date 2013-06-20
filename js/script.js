34a403dd16a10a14c99e0656364c59827c6c6d60
MakerSquare Student <karapendl@gmail.com>
2013-06-20 15:07:27 -0500

    Add animations. Modify script.js and id for images

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.divborder').click(function() {
+        $('#text').addClass('animated wobble');
+    });
+    $('.divborder').click(function() {
+        $('#left').addClass('animated bounceInLeft');
+    });
+    $('.divborder').click(function() {
+        $('#right').addClass('animated bounceInRight');
+    });
+    $('.divborder').click(function() {
+        $('#up').addClass('animated bounceInUp');
+    });
+});
